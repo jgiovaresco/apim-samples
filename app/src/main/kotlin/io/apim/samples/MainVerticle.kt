b314156d9946552bdb51e55d3c720d03799b71bb
Julien Giovaresco <dev@giovaresco.fr>
2023-01-13 11:04:30 +0100

    feat: improve health checks

--- app/src/main/kotlin/io/apim/samples/MainVerticle.kt
+++ app/src/main/kotlin/io/apim/samples/MainVerticle.kt
@@ -6,18 +6,53 @@ import io.apim.samples.websocket.WebSocketServerVerticle
 import io.reactivex.rxjava3.core.Completable
 import io.reactivex.rxjava3.core.Single
 import io.vertx.core.Vertx
+import io.vertx.ext.healthchecks.Status
+import io.vertx.kotlin.core.json.json
+import io.vertx.kotlin.core.json.obj
 import io.vertx.rxjava3.config.ConfigRetriever
 import io.vertx.rxjava3.core.AbstractVerticle
+import io.vertx.rxjava3.ext.healthchecks.HealthChecks
 
 class MainVerticle : AbstractVerticle() {
   override fun rxStart(): Completable {
     val configRetriever = ConfigRetriever.create(vertx)
 
-    return Single.merge(
-      vertx.deployVerticle(WebSocketServerVerticle(configRetriever)),
-      vertx.deployVerticle(RestServerVerticle(configRetriever)),
-      vertx.deployVerticle(GrpcServerVerticle(configRetriever)),
-    ).ignoreElements()
+    return buildHealthChecks(configRetriever)
+      .flatMapCompletable { healthChecks ->
+        Single.merge(
+          vertx.deployVerticle(WebSocketServerVerticle(configRetriever)),
+          vertx.deployVerticle(GrpcServerVerticle(configRetriever)),
+          vertx.deployVerticle(RestServerVerticle(configRetriever, healthChecks)),
+        ).ignoreElements()
+      }
+  }
+
+  private fun buildHealthChecks(configRetriever: ConfigRetriever): Single<HealthChecks> {
+    val client = vertx.createNetClient()
+
+    return configRetriever.config
+      .map {
+        HealthChecks.create(vertx)
+          .register("websocket") { promise ->
+            client.connect(it.getInteger(webSocketPort, WebSocketServerVerticle.DEFAULT_PORT), "0.0.0.0")
+              .doOnSuccess { promise.complete(Status.OK()) }
+              .doOnError { promise.complete(handleConnectionError(it)) }
+              .flatMapCompletable { it.close() }
+              .subscribe()
+          }
+          .register("grpc") { promise ->
+            client.connect(it.getInteger(grpcPort, GrpcServerVerticle.DEFAULT_PORT), "0.0.0.0")
+              .doOnSuccess { promise.complete(Status.OK()) }
+              .doOnError { promise.complete(handleConnectionError(it)) }
+              .flatMapCompletable { it.close() }
+              .subscribe()
+          }
+      }
+  }
+
+  private fun handleConnectionError(throwable: Throwable): Status {
+    val error = json { obj("message" to throwable.message) }
+    return Status.KO(error)
   }
 }
 
--- app/src/main/kotlin/io/apim/samples/rest/RestServerVerticle.kt
+++ app/src/main/kotlin/io/apim/samples/rest/RestServerVerticle.kt
@@ -2,16 +2,19 @@ package io.apim.samples.rest
 
 import io.apim.samples.httpPort
 import io.reactivex.rxjava3.core.Completable
-import io.vertx.ext.healthchecks.Status
 import io.vertx.rxjava3.config.ConfigRetriever
 import io.vertx.rxjava3.core.AbstractVerticle
+import io.vertx.rxjava3.core.Vertx
 import io.vertx.rxjava3.ext.healthchecks.HealthCheckHandler
 import io.vertx.rxjava3.ext.healthchecks.HealthChecks
 import io.vertx.rxjava3.ext.web.Router
 import io.vertx.rxjava3.ext.web.handler.BodyHandler
 import org.slf4j.LoggerFactory
 
-class RestServerVerticle(private val configRetriever: ConfigRetriever) :
+class RestServerVerticle(
+  private val configRetriever: ConfigRetriever,
+  private val healthChecks: HealthChecks = HealthChecks.create(Vertx.vertx())
+) :
   AbstractVerticle() {
   companion object {
     const val DEFAULT_PORT = 8888
@@ -36,10 +39,7 @@ class RestServerVerticle(private val configRetriever: ConfigRetriever) :
     router.route().handler(BodyHandler.create())
     router.route("/echo").handler(::echoHandler)
     router.route("/grpc*").handler(::protobufFileHandler)
-    router.route("/health*").handler(HealthCheckHandler.createWithHealthChecks(healthChecks()))
+    router.route("/health*").handler(HealthCheckHandler.createWithHealthChecks(healthChecks))
     router
   }
-
-  private fun healthChecks(): HealthChecks =
-    HealthChecks.create(vertx).register("status") { promise -> promise.complete(Status.OK()) }
 }
